4d06f8678727e147e6a2dfcd894ad8e1b3a02925
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-06-13 22:21:25 +0300

    minor commit

--- redux-toolkit-demo/app/store.js
+++ redux-toolkit-demo/app/store.js
@@ -2,17 +2,19 @@ const configureStore = require("@reduxjs/toolkit").configureStore;
 const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
+const userReducer = require("../features/user/userSlice");
 
-const logger = reduxLogger.createLogger();
+// const logger = reduxLogger.createLogger();
 
 const store = configureStore({
-   reducer: {
-      cake: cakeReducer,
-      icecream: icecreamReducer,
-   },
-   // middleware: (getDefaultMiddlware) => {
-   //    return getDefaultMiddlware().concat(logger);
-   // },
+    reducer: {
+        cake: cakeReducer,
+        icecream: icecreamReducer,
+        user: userReducer,
+    },
+    // middleware: (getDefaultMiddlware) => {
+    //    return getDefaultMiddlware().concat(logger);
+    // },
 });
 
 module.exports = store;
