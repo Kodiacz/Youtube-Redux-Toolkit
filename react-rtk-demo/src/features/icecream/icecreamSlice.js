46b8c21494e623f38da167e15e114d270ec36cd0
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-06-13 22:21:16 +0300

    copied files

---
+++ react-rtk-demo/src/features/icecream/icecreamSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { ordered as cakeOrdered } from "../cake/cakeSlice";
+
+const initialState = {
+  numOfIcecreams: 20,
+};
+
+const iceCreameSlice = createSlice({
+  name: "icecreame",
+  initialState,
+  reducers: {
+    ordered: (state) => {
+      state.numOfIcecreams--;
+    },
+    restocked: (state, action) => {
+      state.numOfIcecreams += action.payload;
+    },
+  },
+  //    extraReducers: {
+  //       ["cake/ordered"]: (state) => {
+  //          state.numOfIcecreams--;
+  //       },
+  //    },
+  extraReducers: (builder) => {
+    builder.addCase(cakeOrdered, (state) => {
+      state.numOfIcecreams--;
+    });
+  },
+});
+S;
+export default iceCreameSlice.reducer;
+export const { ordered, restocked } = iceCreameSlice.actions;
