bb26096906a26c382f76c3a1c52793f9a701afc7
Simeon Yordanov <simeon.yordanov@amdaris.com>
2023-06-15 23:04:42 +0300

    deleted line

--- react-rtk-demo/src/app/store.js
+++ react-rtk-demo/src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-const reduxLogger = require("redux-logger");
 import cakeReducer from "../features/cake/cakeSlice";
 import icecreamReducer from "../features/icecream/icecreamSlice";
 import userReducer from "../features/user/userSlice";
